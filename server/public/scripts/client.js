56aa0c75b74e7be3fbcc279079653f0f8da0abf2
Justin Cummings <justin.lewis.cummings@gmail.com>
2021-11-12 10:24:13 -0600

    guessing game starter

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,79 @@
 $(document).ready(handleReady);
-
+let randomNumber = 0;
 function handleReady() {
-  console.log("jquery is loaded!")
+  console.log("jquery is loaded!");
+  askForRandomNumber();
+  $('#submit-guess').on('click', collectInputs);
+  $('#reset-button').on('click', resetHandler);
+}
+
+let guessCounter = 0;
+
+// Collect Inputs Function
+function collectInputs () {
+
+  // Capture inputs in variables
+  let justinGuess = { guess :$('#justin-input').val(), name: 'justin'};
+  let zaneGuess = { guess :$('#zane-input').val(), name: 'zane'};
+  let sparkyGuess = { guess :$('#sparky-input').val(), name: 'sparky'};
+
+  console.log('Justins Guess:', justinGuess);
+  console.log('Zanes Guess:', zaneGuess);
+  console.log('Sparkys Guess:', sparkyGuess);
+
+  // Build array of guesses
+  let guessesArray = [ justinGuess, zaneGuess, sparkyGuess ];
+
+  // Iterate through guessArray and check for matches
+  for (guess of guessesArray){
+
+    if (Number(guess.guess) === Number(randomNumber)){
+      alert(`${guess.name} has won!`); 
+    } 
+    else if(Number(guess.guess) > Number(randomNumber)){
+      console.log(`in greater than else if- Guess: ${guess.guess} Number from Server: ${randomNumber}`);
+      $(`#${guess.name}-last-guess`).empty();
+      $(`#${guess.name}-last-guess`).append('was too high!');
+    } 
+    else if(Number(guess.guess) < Number(randomNumber)){
+      console.log(`In less than else if- Guess: ${guess.guess} Number from Server: ${randomNumber}`);
+      $(`#${guess.name}-last-guess`).empty();
+      $(`#${guess.name}-last-guess`).append('was too low!');
+    }
+
+    // Clear input fields
+    $('#justin-input').val('');
+    $('#zane-input').val('');
+    $('#sparky-input').val('');
+
+    guessCounter ++;
+
+  }
+}
+
+
+
+  function resetHandler () {
+    $(`#justin-last-guess`).empty();
+    $(`#zane-last-guess`).empty();
+    $(`#sparky-last-guess`).empty();
+    askForRandomNumber();
+    guessCounter=0;
+  }
+
+
+  
+
+
+function askForRandomNumber() {
+  $.ajax({
+    method: 'GET',
+    url: '/numbersGame'
+  }).then((response) => {
+    randomNumber = response;
+    console.log(randomNumber);
+    console.log('response', response);
+  }).catch((error) => {
+    console.log('error', error);
+  })
 }
--- server/server.js
+++ server/server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
-
+const min = 0
+const max = 25
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
 
@@ -10,8 +13,14 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
+app.get('/numbersGame', (req, res )=> {
+  console.log('in /numbersGame');
+  res.send(Math.floor(Math.random()*(max-min+1)+min).toString());
+});
 
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
 })
+// CODE FOR OUR RNG
+// Math.floor(Math.random()*(max-min+1)+min)
